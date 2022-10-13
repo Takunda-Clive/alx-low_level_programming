397aa3b4e0c2ac095a66637443e6d948dd6858a0
Takunda-Clive <chigangaidzetakundaclive@gmail.com>
2022-10-13 14:09:19 -0700

    c7

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of a char : %i bytes(s)\n", Sizeof(char));
+	printf("Size of an int : %i bytes(s)\n", Sizeof(int));
+	printf("Size of a long int : %i bytes(s)\n", Sizeof(long int));
+	printf("Size of a float : %i bytes(s)\n", Sizeof(float))i;
+	return (0);
+}
+
