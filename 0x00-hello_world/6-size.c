d0f87eb6f5df4bbddd3d7b6af696a1bf18cf6c0c
Takunda-Clive <chigangaidzetakundaclive@gmail.com>
2022-10-13 14:18:28 -0700

    c7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,6 +8,7 @@ int main(void)
 	printf("Size of a char: %i bytes(s)\n", Sizeof(char));
 	printf("Size of an int: %i bytes(s)\n", Sizeof(int));
 	printf("Size of a long int: %i bytes(s)\n", Sizeof(long int));
+	printf("Size of a long long int: %i bytes(s)\n", Sizeof(long long int));
 	printf("Size of a float: %i bytes(s)\n", Sizeof(float))i;
 	return (0);
 }
