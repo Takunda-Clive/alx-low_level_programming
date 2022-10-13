ddb4ea056552980a5d627943a5956845c03451fa
Takunda-Clive <chigangaidzetakundaclive@gmail.com>
2022-10-13 14:14:51 -0700

    c7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-	printf("Size of a char : %i bytes(s)\n", Sizeof(char));
-	printf("Size of an int : %i bytes(s)\n", Sizeof(int));
-	printf("Size of a long int : %i bytes(s)\n", Sizeof(long int));
-	printf("Size of a float : %i bytes(s)\n", Sizeof(float))i;
+	printf("Size of a char: %i bytes(s)\n", Sizeof(char));
+	printf("Size of an int: %i bytes(s)\n", Sizeof(int));
+	printf("Size of a long int: %i bytes(s)\n", Sizeof(long int));
+	printf("Size of a float: %i bytes(s)\n", Sizeof(float))i;
 	return (0);
 }
 
