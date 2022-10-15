247a6128d8e464c958b4e68a98433b9493d1b419
Takunda-Clive <chigangaidzetakundaclive@gmail.com>
2022-10-14 19:50:01 -0700

    view6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers from 0-9 (base of 10)
+ * Return: 0 for success
+ **/
+int main(void)
+{
+	int i;
+
+	i = 48;
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
