5364cbc2bf22485cec4e743f33a28a4b03f1b584
Takunda-Clive <chigangaidzetakundaclive@gmail.com>
2022-10-14 20:07:41 -0700

    view8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Prints all the numbers of base 16 in lowercase
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i, a;
+
+	i = 48;
+	a = 'a';
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	while (a <= 'f')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar(10);
+	return (0);
+}
