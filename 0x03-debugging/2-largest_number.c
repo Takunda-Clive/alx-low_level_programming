3d6267798a6966bc0ab32b56406b687f3e3a160e
Takunda-Clive <chigangaidzetakundaclive@gmail.com>
2022-10-20 06:32:27 -0700

    dbg2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -4,12 +4,11 @@
  *main - causes an infinite loop
  *Return: 0
  */
- 
 int main(void)
 
 {
 	int i;
-	
+
 	printf("Infinite loop incoming :(\n");
 	i = 0;
 	/*
@@ -19,6 +18,6 @@ int main(void)
 	 * }
 	 */
 	printf("Infinite loop avoided! \\o/\n");
-	
+
 	return (0);
 }
