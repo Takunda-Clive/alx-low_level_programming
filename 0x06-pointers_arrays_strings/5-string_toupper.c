7122b9211b494e123951e17038406f377bff0138
Takunda-Clive <chigangaidzetakundaclive@gmail>
2022-10-26 21:51:35 -0700

    M.A.PS.S5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -14,7 +14,7 @@ char *string_toupper(char *s)
 	while (*(s + count) != '\0')
 	{
 		if ((*(s + count) >= 97) && (*(s + count) <= 122))
-			*(s + count) = *(s + count) - 3;
+			*(s + count) = *(s + count) - 32;
 		count++;
 	}
 	return (s);
